d6aae39523a59577c073c9a64231e118324e4991
Michael Paul <michaelpaulwem@gmail.com>
2025-09-19 10:24:17 +0300

    File size method added

--- script.py
+++ script.py
@@ -4,17 +4,19 @@ import cv2 as cv
 dir = r"D:/video/video/"
 
 l = [dir + os.listdir(dir)[i] for i in range(len(os.listdir(dir)))]
+d = []
 
 def with_opencv(l):
-    d = []
-    for i in l:
+    for i in l:        
         cap = cv.VideoCapture(i)
         fps = cap.get(cv.CAP_PROP_FPS)
         frame_count = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
         duration = frame_count/fps
         minutes = str(int(duration/60))
         seconds = str(int(duration%60))
-        d.append(i + ", длительность: " + minutes + ":".rstrip() + seconds.lstrip())
+        d.append(i + ", размер: " + str(os.path.getsize(i)//(1024**2)) + " мб" + ", длительность: " + minutes + ":".rstrip() + seconds.lstrip())
     
-    print(d)
-    return minutes, seconds
+    # print(d)
+    return d
+
+# with_opencv(l)
