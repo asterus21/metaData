b78c3d7012326ab8ec04e57e5f719c7a889329d2
Mikhail Palvov <mpavlov@megaputer.ru>
2025-11-12 16:01:10 +0300

    Code style changed

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ import cv2 as cv
 PATH = r"D:/video/video/"
 
 
-def get_video_duration(video_file: bytes) -> tuple:
+def get_video_duration(file_name: str) -> tuple:
     """Returns a number of frames and frames per second for a video file."""
-    cap = cv.VideoCapture(video_file)
+    cap = cv.VideoCapture(file_name)
     frames_per_second = cap.get(cv.CAP_PROP_FPS)
     frames_count = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
+
     return frames_count, frames_per_second
 
 
 def main(folder):
-    """Returns metadata for all video files in a folder."""
-    data = []
+    """Returns metadata for all video files in a folder."""    
     try:
         os.path.exists(PATH)
         if len(os.listdir(PATH)) == 0:
@@ -28,6 +28,7 @@ def main(folder):
     for file in files:
             if not file.endswith('.mov'):
                 raise Exception('Error: the file(s) format is not supported. All files must be of the same format, e.g. "video.mov"')
+    data = []
     for file in files:
         frames_count, frames_per_second = get_video_duration(file)
         duration = frames_count / frames_per_second      
@@ -46,7 +47,9 @@ def main(folder):
                 )
             )
     print(data)
+
     return data
+
 
 if __name__ == '__main__':
     main(PATH)
