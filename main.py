f923651f1511b532f17884bde5df4af47a81a302
Mikhail Palvov <mpavlov@megaputer.ru>
2025-11-20 14:35:19 +0300

    Files check given as a separated function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import os
 import cv2 as cv
 
@@ -25,8 +26,8 @@ def get_video_duration(duration: float) -> str:
     return length
 
 
-def main(folder):
-    """Returns metadata for all video files in a folder."""
+def get_files_list(folder: str) -> list:
+    """Checks the files in hand, i.e. whether they exist and their type."""
     try:
         os.path.exists(folder)
         number_of_files = len(os.listdir(folder))
@@ -40,6 +41,12 @@ def main(folder):
     for file in files:
             if not file.endswith('.mov'):
                 raise Exception('Error: the file(s) format is not supported. All files must be of the same format, e.g. "video.mov"')
+    
+    return files
+
+
+def main(files):
+    """Returns metadata for all video files in a folder."""    
     data = []
     for file in files:
         size = str(os.path.getsize(file) // (1024 ** 2))
@@ -55,11 +62,10 @@ def main(folder):
                     length
                     ]
                 )
-            )
-    # TODO: add Pandas to create a table of files data
-    print(data)
+            )    
+    # print(data)
     return data
 
 
-if __name__ == '__main__':
-    main(PATH)
+if __name__ == '__main__':    
+    main(get_files_list(PATH))
